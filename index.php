b254f865d32bb93e598d6afcdeafe3de267cabeb
ramity <lcb005@uark.edu>
2016-05-31 12:00:54 -0500

    init commit

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+	<head>
+		<title>Framework Project - a</title>
+		<meta charset="UTF-8">
+		<meta name="description" content="">
+		<meta name="keywords" content="">
+		<meta name="author" content="Lewis DB2 Brown">
+		<link href='https://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" type="text/css" href="https://ramity.com/fw/a/min.css">
+		<link rel="stylesheet" type="text/css" href="https://ramity.com/fw/a/main.css">
+	</head>
+	<body>
+		<div id="top">
+			<div id="topinr">
+				<a href="#">
+					<div id="toplogo"></div>
+				</a>
+				<div id="topmenu">
+					<div class="topmenuitem">
+						<a class="topmenuitem" href="#">Log Out</a>
+					</div>
+					<div class="topmenuitem">
+						<a class="topmenuitem" href="#">
+							You
+						</a>
+					</div>
+					<div class="topmenuitem">
+						<a class="topmenuitem" href="#">Followers</a>
+					</div>
+					<div class="topmenuitem">
+						<a class="topmenuitem" href="#">Inbox</a>
+					</div>
+					<div class="topmenuitem">
+						<a class="topmenuitem" href="#">Notifications</a>
+					</div>
+					<div class="topmenuitemactive">
+						<a class="topmenuitem" href="#">Stream</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="content">
+			<div id="contentinr">
+				<h1 class="blogheader">
+					Introducing the Challenge : Ramity.com
+				</h1>
+				<p class="blogtextfull">
+					During the last few years, ramity.com has been the location for all my hopes, dreams, and aspirations to become a reality.
+					While on this journey of programming and design, I've learned a multitude of skills and tricks to help supplement my more difficult projects come to light.
+					Everything from e-Commerce to blogs to basic forums and thread websites to full fledged social media websites.
+					You name it, I've tried it at-least once.
+					But here I am again wielding the knowledge I've learned from all my failures and successes.
+					I'm here to make my own history as a full stack web developer, broaden my horizons, and maybe thicken that wallet of mine.
+					But there's a catch 21 at my own expense: college.
+					So here I am, trying to make something in roughly less than 2 months that can rival all other social media networks at this time.
+					Myself being the only programmer. This could be an interesting challenge... Let's start shall we?
+					<br><br>
+					-<i><font color="#728dfd">Site Dev</font> Lewis 'DB2' Brown</i>
+				</p>
+			</div>
+		</div>
+	</body>
+</html>
